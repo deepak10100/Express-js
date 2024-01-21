41415b3eda439613db5de9812aa0acb6315fd868
Deepak Malakar <80341169+deepak10100@users.noreply.github.com>
2024-01-22 01:17:48 +0530

    22 day

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express')
+const bodyParser= require('body-parser')
+const path = require('path')
 const app = express()
 const port = 3000
+app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse application/json
+app.use(bodyParser.json())
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname+'/index.html'))
 })
-app.get('/des/:name/:id', (req, res) => {
-  res.send('the describation!'+ req.params.name + req.params.id)
-})
-app.get('*', (req, res) => {
-  res.send('not found' + Date.now())
+app.post('/', (req, res) => {
+  // const username =req.body.name
+  console.log(req.body)
+  res.send(`name: ${req.body.name}`)
 })
 
 app.listen(port, () => {

31221bbaf416eddbe022d3d87ac6b69781fb1fc2
Deepak Malakar <80341169+deepak10100@users.noreply.github.com>
2024-01-25 23:09:16 +0530

    25 day

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const express = require('express')
-const mongoose = require('mongoose');
 const app = express()
 const port = 3000
+const path = require('path')
+var bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/mydata')
-  .then(() => console.log('Connected!'));
+.then(() => console.log('Connected!'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs')
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const BlogPost = new Schema({
-  author: ObjectId,
+const messageSchema = new Schema({
   name: String,
-  age: Number,
+  email: String,
   
 });
-
-const MyModel = new mongoose.model('Ticket', BlogPost);
-let add = async()=>{
-  let ss = await MyModel.updateMany({age:{$gt:15}},{$set:{age:19}})
-  console.log(ss)
-}
-add()
-
-
+const Message = mongoose.model('Message', messageSchema);
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("index")
+})
+app.get('/login', (req, res) => {
+  res.render("login")
+})
+app.post('/login', (req, res) => {
+  res.render("login")
+})
+app.post('/add', async(req, res) => {
+    let {name,email} = req.body
+    await Message.create({name,email})
+    res.redirect('/')
 })
 
 app.listen(port, () => {

03e89b3392d09364b13646523ec275f2bda51fb1
Deepak Malakar <80341169+deepak10100@users.noreply.github.com>
2024-01-27 01:56:23 +0530

    27 day

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
 var bodyParser = require('body-parser')
 const mongoose = require('mongoose');
+const { sign } = require('crypto')
 mongoose.connect('mongodb://127.0.0.1:27017/mydata')
 .then(() => console.log('Connected!'));
 // parse application/x-www-form-urlencoded
@@ -16,25 +19,69 @@ app.set('view engine', 'ejs')
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const messageSchema = new Schema({
+const signupSchema = new Schema({
   name: String,
   email: String,
+  password:String,
+  cpassword:String,
   
 });
-const Message = mongoose.model('Message', messageSchema);
+const Signup = mongoose.model('Signup', signupSchema);
+let auth =(req,res,next)=>{
+  let {token} = req.cookies
+  if (token) {
+    next()
+  }
+  else{
+    res.redirect('/login')
+  }
+}
 app.get('/', (req, res) => {
   res.render("index")
 })
-app.get('/login', (req, res) => {
+
+app.get('/login',  (req, res) => {
   res.render("login")
 })
-app.post('/login', (req, res) => {
-  res.render("login")
+app.get('/logout', (req, res) => {
+  res.cookie('token',null,{
+    expires:new Date(Date.now())
+  })
+  res.render("logout")
 })
-app.post('/add', async(req, res) => {
-    let {name,email} = req.body
-    await Message.create({name,email})
+app.post('/login', async (req, res) => {
+  let{email,password} = req.body
+  let user = await Signup.findOne({email})
+  
+  if (!user) {
     res.redirect('/')
+  }
+  else if (user.password===password) {
+    res.cookie('token','iamin')
+    res.redirect('/des')
+    console.log(user)
+  } 
+   else {
+    res.render('login')
+  }
+
+})
+app.get('/des',auth, (req, res) => {
+  res.render("des")
+})
+app.post('/', async(req, res) => {
+    let {name,email,password,cpassword} = req.body
+    let user =await Signup.findOne({email})
+   if (user){
+      res.redirect('/login')
+   }else if (password===cpassword) {
+      await Signup.create({name,email,password,cpassword})
+      res.redirect('/login')
+    }
+    else{
+      console.log("password not match")
+
+    }
 })
 
 app.listen(port, () => {
